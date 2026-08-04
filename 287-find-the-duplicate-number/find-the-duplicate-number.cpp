aa08e6bee53e8e6c80098f9e2726c332c1da4aab
Kartik Raj <crazystatusbhai@gmail.com>
2026-08-04 18:13:31 +0530

    Time: 0 ms (100.00%) | Memory: 65.1 MB (42.15%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int n = nums.size() - 1;
-        //range is 1 to n;
-         sort(nums.begin(),nums.end());
-         int temp = -1;
-         for(auto it: nums){
-            if(temp == it){
-                return it;
-            }temp = it;
-         }return temp;
+        // Phase 1: Find the intersection point of the two runners in the cycle
+        int slow = nums[0];
+        int fast = nums[0];
+        
+        do {
+            slow = nums[slow];          // Tortoise moves 1 step
+            fast = nums[nums[fast]];    // Hare moves 2 steps
+        } while (slow != fast);
+        
+        // Phase 2: Find the entrance to the cycle (the duplicate number)
+        slow = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        
+        return slow;
     }
 };
