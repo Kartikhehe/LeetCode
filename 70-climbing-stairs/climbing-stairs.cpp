e6404f541b0d47fdf1397bb93b358f3d588d199b
Kartik Raj <crazystatusbhai@gmail.com>
2026-09-03 19:21:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (20.82%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -9,7 +9,15 @@ public:
     }
 
     int climbStairs(int n) {
+        if(n==2)return 2;
+        if(n==1)return 1;
         vector<int> dp(n+1, -1);
-        return climbStairs1(n, dp);
+        
+        dp[1] = 1; 
+        dp[2] = 2;
+        for(int i = 3; i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
